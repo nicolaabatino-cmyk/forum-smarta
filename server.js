b043b6efdedf8bc34b6dc929393c3612ec4f5fde
nicolaabatino-cmyk <nicolaabatino@alberghieropesaro.it>
2026-05-09 10:37:08 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -48,7 +48,10 @@ try {
 let transporter;
 if (process.env.EMAIL_USER && process.env.EMAIL_PASS) {
     transporter = nodemailer.createTransport({
-        service: 'gmail',
+        host: 'smtp.gmail.com',
+        port: 587,
+        secure: false, // usa STARTTLS su porta 587
+        requireTLS: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS
